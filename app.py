6964e5ae4ae2bfa677df086373ad32c4d1259bea
Nicola Kalak <Nicolas12345@gmail.com>
2021-03-24 23:06:32 +0200

    add pre/post portfolio trend

--- app.py
+++ app.py
@@ -5,11 +5,33 @@ import pandas as pd
 from flask import Flask
 
 app = Flask(__name__, static_url_path='/static/')
+symbols_qty = {}
+API = 'https://query1.finance.yahoo.com/v7/finance/quote?&symbols='
 
 
 def get_table():
-    r = requests.get(os.environ.get('portfolio_link'))
+    r = requests.get(os.getenv('portfolio_link'))
     return r.text
+
+
+def calc_trend(market_state, data):
+    result = {'marketState': market_state, 'trend': 0}
+    key = market_state.lower() + 'MarketChange'
+    for d in data:
+        if key in d:
+            result['trend'] += d[key] * symbols_qty[d['symbol']]
+    return result
+
+
+@app.route('/trend')
+def get_trend():
+    r = requests.get(API + ','.join(symbols_qty.keys()))
+    data = json.loads(r.text)['quoteResponse']['result']
+    if len(data) > 0:
+        return calc_trend(data[0]['marketState'], data)
+    else:
+        raise RuntimeError()
+
 
 
 @app.route('/portfolio')
@@ -21,6 +43,8 @@ def get_data():
         to_json(orient='records'))
     for d in data:
         d['percent_change'] = (float(d['Change']) / (float(d['Last']) - float(d['Change']))) * 100
+    global symbols_qty
+    symbols_qty = dict(map(lambda kv: (kv['Symbol'], kv['Qty']), data))
     return json.dumps(data)
 
 
