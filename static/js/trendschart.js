93388001ff512f4dfa2b9c7b4d4e8359dd6c33ca
nkalak <nicolas12345@gmail.com>
2021-05-27 13:40:41 +0300

    update chart on table refresh

--- static/js/trendschart.js
+++ static/js/trendschart.js
@@ -17,45 +17,52 @@ function trend_stats(trendObj) {
     $("#market-stats").show(200);
     console.log(trendObj);
 }
-
-function trends() {
+const trendsObj = {}
+let chart;
+function update_trends() {
+    $.get("trends", function (trends) {
+        for (let key in trends) {
+            trendsObj[key] = trends[key];
+        }
+        chart.update();
+    });
+}
+function init_chart() {
     const down = (ctx, value) => ctx.p0.parsed.y > ctx.p1.parsed.y ? value : undefined;
     const up = (ctx, value) => ctx.p0.parsed.y < ctx.p1.parsed.y ? value : undefined;
     const skipped = (ctx, value) => new Date(ctx.p1.parsed.x).getDay() - new Date(ctx.p0.parsed.x).getDay() > 0 ? value : undefined;
-
-
-    $.get("trends", function (trends) {
-        const data = {
-            datasets: [{
-                label: "trends",
-                data: trends,
-                segment: {
-                    borderColor: ctx => skipped(ctx, 'rgb(0,0,0,0.2)') ||up(ctx, 'rgba(72,141,22,0.2)') || down(ctx, 'rgb(234,15,15)'),
-                    borderDash: ctx => skipped(ctx, [6, 6]),
-                }
-            },]
-        };
-        const config = {
-            type: 'line',
-            data: data,
-            options: {
-                scales: {
-                    x: {
-                        display: false,
-                        type: 'time',
-                        time: {
-                            parser: 'YYYYMMDDTHH:mm:ss',
-                            minUnit: 'minute'
-                        },
+    const data = {
+        datasets: [{
+            label: "trends",
+            data: trendsObj,
+            segment: {
+                borderColor: ctx => skipped(ctx, 'rgb(0,0,0,0.2)') ||up(ctx, 'rgba(72,141,22,0.2)') || down(ctx, 'rgb(234,15,15)'),
+                borderDash: ctx => skipped(ctx, [6, 6]),
+            }
+        },]
+    };
+    const config = {
+        type: 'line',
+        data: data,
+        options: {
+            scales: {
+                x: {
+                    display: false,
+                    type: 'time',
+                    time: {
+                        parser: 'YYYYMMDDTHH:mm:ss',
+                        minUnit: 'minute'
                     },
                 },
-                plugins: {
-                    legend: {
-                        display: false
-                    }
+            },
+            plugins: {
+                legend: {
+                    display: false
                 }
             }
-        };
-        new Chart($('#trends'), config);
-    });
+        }
+    };
+    chart = new Chart($('#trends'), config);
+    update_trends();
+
 }
