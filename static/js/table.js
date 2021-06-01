c01d489298dff96cfb40e4ad1f51a3a53212020f
nkalak <nicolas12345@gmail.com>
2021-06-01 12:03:43 +0300

    add info to yahoo finance for more info

--- static/js/table.js
+++ static/js/table.js
@@ -1,6 +1,9 @@
 
 function detailFormatter(index, row) {
-    let html = '<div class="container-fluid"><div class="row justify-content-start" id="ticker-' + row.Symbol +'"></div></div>'
+    let html = '<div class="container-fluid">' +
+        '<div class="row justify-content-start" id="ticker-' + row.Symbol +'"></div>' +
+        '<div class="row justify-content-start" id="ticker-' + row.Symbol +'-link"></div>' +
+        '</div>'
     $.get("ticker/" + row.Symbol, function (stock) {
         let container = $("#ticker-" + row.Symbol);
         container.append('<dl class="row">'
@@ -15,6 +18,10 @@ function detailFormatter(index, row) {
             + getDetailedRow('change (%)', (stock[market_state + 'MarketChangePercent']), roundPercent, true )
             + '</dl>'
         )
+        $("#ticker-" + row.Symbol + '-link').html(
+            '<a title="more info..." class="link-primary fa-lg" target="_blank" href="https://finance.yahoo.com/quote/' + row.Symbol +'"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-arrow-up-right-circle" viewBox="0 0 16 16">\n' +
+            '  <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.854 10.803a.5.5 0 1 1-.708-.707L9.243 6H6.475a.5.5 0 1 1 0-1h3.975a.5.5 0 0 1 .5.5v3.975a.5.5 0 1 1-1 0V6.707l-4.096 4.096z"/>\n' +
+            '</svg></a>')
     });
     return html;
 }
