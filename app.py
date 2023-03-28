88dfc05abda3644ac53c90794f0e61e506f3afc3
nicolak <nicolas12345@gmail.com>
2023-03-28 12:09:44 +0300

    add more logs

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import http
 import json
 import logging
 import os
@@ -30,9 +31,14 @@ def load_config():
 
 
 def get_portfolio_table():
-    r = requests.get(os.getenv('portfolio_link'),
-                     headers={'User-Agent': 'Meitav-Viewer/{}'.format(os.getenv("HOSTNAME"))})
-    return r.text
+    try:
+        r = requests.get(os.getenv('portfolio_link'),
+                        headers={'User-Agent': 'Meitav-Viewer/{}'.format(os.getenv("HOSTNAME"))})
+        if r.status_code != http.HTTPStatus.OK.value:
+            logger.error("failed to get portfolio from Meitav {} {}".format(r.status_code, r.text))
+        return r.text
+    except ConnectionError as e:
+        logger.error("failed to connect to meitav", e)
 
 
 def add_trend(trends_obj, change_key):
@@ -118,33 +124,37 @@ def ticker_data(name):
 
 @app.route('/portfolio')
 def get_enriched_portfolio() -> List[Stock]:
-    logger.info("request for portfolio {}".format(request.headers))
+    logger.info("request for portfolio from: {} {}".format(request.headers.get("X-Real-Ip"),
+                                                           request.headers.get("User-Agent")))
     stocks_cache.clear()
     portfolio: List[Stock] = get_portfolio_data()
     watch_list = set(config["watch_list"])
     logger.info("watch list is {}".format(watch_list))
-    r = requests.get(API + ','.join(set().union(map(lambda s: s.symbol, portfolio), watch_list)),
-                     headers=config["api_headers"])
-    if r.status_code == 200:
-        yahoo_data = json.loads(r.text)['quoteResponse']['result']
-        for stock in portfolio:
-            stock.set_api_data(next(filter(lambda s: s['symbol'] == stock.symbol, yahoo_data)))  # expect only one
-            stocks_cache.append(stock)
-        for watch_stock in watch_list:
-            api_data = next(filter(lambda s: s['symbol'] == watch_stock, yahoo_data))  # expect only one
-            stock = Stock({
-                'Symbol': api_data['symbol'], 'Qty': 0,
-                'Gain': None,
-                'Day\'s Value': round(api_data.get(get_market_state_key() + 'MarketChange', 0), 2),
-                'Entry Type': 'W',
-                'Last': api_data.get(get_market_state_key() + 'MarketPrice', 'nan'),
-                'Change': api_data.get(get_market_state_key() + 'MarketChange', 'nan')})
-            stock.set_api_data(api_data)
-            stocks_cache.append(stock)
-        return stocks_cache
-    else:
-        logger.error("failed to retrevie data from yahoo {}", r.text)
-        return stocks_cache
+    try:
+        r = requests.get(API + ','.join(set().union(map(lambda s: s.symbol, portfolio), watch_list)),
+                         headers=config["api_headers"])
+        if r.status_code == http.HTTPStatus.OK.value:
+            yahoo_data = json.loads(r.text)['quoteResponse']['result']
+            for stock in portfolio:
+                stock.set_api_data(next(filter(lambda s: s['symbol'] == stock.symbol, yahoo_data)))  # expect only one
+                stocks_cache.append(stock)
+            for watch_stock in watch_list:
+                api_data = next(filter(lambda s: s['symbol'] == watch_stock, yahoo_data))  # expect only one
+                stock = Stock({
+                    'Symbol': api_data['symbol'], 'Qty': 0,
+                    'Gain': None,
+                    'Day\'s Value': round(api_data.get(get_market_state_key() + 'MarketChange', 0), 2),
+                    'Entry Type': 'W',
+                    'Last': api_data.get(get_market_state_key() + 'MarketPrice', 'nan'),
+                    'Change': api_data.get(get_market_state_key() + 'MarketChange', 'nan')})
+                stock.set_api_data(api_data)
+                stocks_cache.append(stock)
+        else:
+            logger.error("failed to retrieve data from yahoo {}", r.text)
+    except ConnectionError as e:
+        logger.error("connection Error while getting API", e)
+
+    return stocks_cache
 
 
 def get_portfolio_data() -> List[Stock]:
