577837c8f4daa36d8378aff3a6389e3ffbe3fdbd
nicolak <nicolas12345@gmail.com>
2023-05-29 22:45:14 +0300

    move css to file

--- app.py
+++ app.py
@@ -198,6 +198,11 @@ def send_js(path):
     return send_from_directory('static/js', path)
 
 
+@app.route('/css/<path:path>')
+def send_css(path):
+    return send_from_directory('static/css', path)
+
+
 @app.route('/favicon/<path:icon>')
 def favicon(icon):
     if icon == "site.webmanifest":
